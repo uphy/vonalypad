a1f729abf19b002672ae9841ea247fd520084b29
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2021-05-11 23:10:14 +0900

    Introduce markdown note.

--- frontend/src/components/Note.vue
+++ frontend/src/components/Note.vue
@@ -1,28 +1,36 @@
 <template>
-  <v-container fluid>
-    <v-card>
-      <v-card-title>Note</v-card-title>
-      <v-card-text>
-        <v-textarea
-          auto-grow
-          v-model="note"
-          rows="1"
-        ></v-textarea>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn
-          depressed
-          @click="update"
-        >
-          Save
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-container>
+  <v-card :loading="loading">
+    <v-card-title>Note</v-card-title>
+    <v-card-text>
+      <v-textarea
+        auto-grow
+        v-model="note"
+        rows="1"
+        v-if="editorMode"
+        @blur="blurred"
+      ></v-textarea>
+      <div
+        v-html="compiledMarkdown"
+        @click="editorMode=true"
+        v-else
+        style="min-height:1rem"
+      />
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        depressed
+        @click="update"
+        v-if="editorMode"
+      >
+        Save
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
 import axios from "axios";
+import marked from "marked";
 
 export default {
   props: {
@@ -31,6 +39,8 @@ export default {
   data: () => {
     return {
       note: "",
+      loading: false,
+      editorMode: false,
     };
   },
   async mounted() {
@@ -41,15 +51,39 @@ export default {
       await this.fetch();
     },
   },
+  computed: {
+    compiledMarkdown() {
+      return marked(this.note);
+    },
+  },
   methods: {
+    blurred() {
+      window.setTimeout(() => {
+        this.editorMode = false;
+      }, 100);
+    },
     async fetch() {
-      const resp = await axios.get(`./api/recipes/${this.recipeId}/note`);
-      this.note = resp.data;
+      this.loading = true;
+      try {
+        const resp = await axios.get(`./api/recipes/${this.recipeId}/note`);
+        this.note = resp.data;
+      } finally {
+        this.loading = false;
+      }
+      if (this.note.length === 0) {
+        this.editorMode = true;
+      }
     },
     async update() {
-      await axios.put(`./api/recipes/${this.recipeId}/note`, {
-        note: this.note,
-      });
+      this.loading = true;
+      try {
+        await axios.put(`./api/recipes/${this.recipeId}/note`, {
+          note: this.note,
+        });
+      } finally {
+        this.loading = false;
+      }
+      this.editorMode = false;
     },
   },
 };
